2b5a170e2dcd248b30f3851c98996c833b85d768
anjalib24 <anjalibmca24@ipsacademy.org>
2024-01-16 21:42:53 -0600

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+// import Logo form './Logo/imagesin.jpeg';
 
 function App() {
+  const [login,setlogin]=useState(true);
+
+  const loginSwitch=()=>{
+    setlogin(!login)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="user">
+         <img src="https://1000logos.net/wp-content/uploads/2017/02/Instagram-Logo.png" alt=""/>
+        <input hidden={login} type="text" name="" placeholder='mobile number or gmail' id=""/>
+        <input hidden={login} type="text" name="" placeholder='full name' id=""/>
+        <input type="email" name="" placeholder='phone number,username or email' id=""/>
+        <input type="password" name="" placeholder='password' id=""/>
+         <button>{login?"signin":"sign up"}</button>
+
+        <div id="footer">
+{login?"dont have account":"have an account"}
+<span onClick={loginSwitch} >{login?"sign up":"log in"}</span>
+
+        </div>
+
+
+          
+
+
+           
+
+
+      </div>
+
+
+
+
+      
     </div>
   );
 }
